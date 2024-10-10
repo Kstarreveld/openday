1f19109ae80dd88d96d39c77e7fad4c5d432d181
kstarreveld <kstarreveld@deltion.nl>
2024-10-10 13:40:22 +0200

    https

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
-
+var https = require('https');
+var fs = require('fs');
+var app = express();
 
 var indexRouter = require('./index');
-
-var app = express();
 
 app.use('/', indexRouter);
 app.use(express.static('public'));
@@ -11,8 +11,20 @@ app.use(express.static('public'));
 
 const port = 3000;
 
- 
+ /*
 app.listen(port, () => {
     console.log(`Server running at port:${port}`);
 }); 
-  
+  */
+
+// This line is from the Node.js HTTPS documentation.
+var options = {
+  key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
+  cert: fs.readFileSync('test/fixtures/keys/agent2-cert.cert')
+};
+
+// Create an HTTPS service identical to the HTTP service.
+https.createServer(options, app).listen(port,()=> 
+{
+    console.log(`Server started on port ${port}`);
+});
